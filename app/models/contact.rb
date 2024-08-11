aaa2d0a2d6bddbdf169c8ff6360f77f09e44a4d8
Alex <alex.beciana@gmail.com>
2024-08-10 22:49:53 -0600

    Added validation and association

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,8 @@
 class Contact < ApplicationRecord
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
     has_many :emails, dependent: :destroy
+
+    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+    validates :email, uniqueness: true
 end
